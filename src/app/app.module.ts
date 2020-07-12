9cdf7b70381afbdd8798267342d5c3b47a3d29da
bego <begomontes7@gmail.com>
2020-07-11 20:08:44 -0500

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CorouselComponent } from './corousel/corousel.component';
+import { ActividadesComponent } from './actividades/actividades.component';
+import { ParallaxComponent } from './parallax/parallax.component';
+import { CardsComponent } from './cards/cards.component';
+import { CalendarioComponent } from './calendario/calendario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    CorouselComponent,
+    ActividadesComponent,
+    ParallaxComponent,
+    CardsComponent,
+    CalendarioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/corousel/corousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-corousel',
+  templateUrl: './corousel.component.html',
+  styleUrls: ['./corousel.component.scss']
+})
+export class CorouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
